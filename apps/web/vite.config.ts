272264ebaa69dbed836c6502dc9fc763edc0f106
zain-23 <zaingit780@gmail.com>
2026-08-02 23:24:48 +0500

    chore: remove Cloudflare Vite plugin from dependencies; add nitro 3.0.260610-beta to package.json; update Vite configuration to integrate nitro plugin; delete wrangler configuration file

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,13 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
-	plugins: [
-		devtools(),
-		tailwindcss(),
-		cloudflare({ viteEnvironment: { name: "ssr" } }),
-		tanstackStart(),
-		viteReact(),
-	],
+	plugins: [devtools(), tailwindcss(), nitro(), tanstackStart(), viteReact()],
 });
 
 export default config;
